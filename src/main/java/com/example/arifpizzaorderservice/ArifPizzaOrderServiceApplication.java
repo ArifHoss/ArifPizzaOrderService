ff089d8fdcc6acbe072a7f4623b731b78f00737c
Arif <hossaininfo@ymail.com>
2021-08-25 12:04:20 +0200

    RabbitMQConfiguration, application.properties,dependencies on pom.xml, OrderController

--- src/main/java/com/example/arifpizzaorderservice/ArifPizzaOrderServiceApplication.java
+++ src/main/java/com/example/arifpizzaorderservice/ArifPizzaOrderServiceApplication.java
@@ -1,13 +1,26 @@
 package com.example.arifpizzaorderservice;
 
+import com.example.arifpizzaorderservice.entities.Pizza;
+import com.example.arifpizzaorderservice.repository.PizzaRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ArifPizzaOrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ArifPizzaOrderServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init(PizzaRepository pizzaRepository){
+        return args -> {
+            if( pizzaRepository.count() == 0) {
+                pizzaRepository.save(new Pizza("Hawaii",80,"Ost,Tomatsås,Skinka,Ananas,Bannan"));
+            }
+        };
     }
 
 }
--- src/main/java/com/example/arifpizzaorderservice/controllers/OrderController.java
+++ src/main/java/com/example/arifpizzaorderservice/controllers/OrderController.java
@@ -1,5 +1,27 @@
 package com.example.arifpizzaorderservice.controllers;
 
+import com.example.arifpizzaorderservice.messagingrabbit.RabbitMQConfiguration;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public OrderController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @PostMapping("/orders")
+    String orderPizza(@RequestBody Order newOreder) {
+        var orderId = (int) (Math.random() * 10000);
+        rabbitTemplate.convertAndSend(RabbitMQConfiguration.topicExchangeName,
+                "pizza-key", "Vegetable special");
+
+        return "Thank you for your order " + orderId;
+    }
 
 }
